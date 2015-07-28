47c22079beb1733d01f19e5e288aa566e1cd6e60
Ivan Malison <imalison@uber.com>
2015-07-27 22:49:42 -0700

    fixups for qwerty--imalison

--- firmware/keyboard/ergodox/options.mk
+++ firmware/keyboard/ergodox/options.mk
@@ -21,13 +21,14 @@ F_CPU := 16000000
 # processor speed, in Hz; max value is 16000000 (16MHz); must match
 # initialization in source
 
-KEYBOARD_LAYOUT := qwerty--ben
+KEYBOARD_LAYOUT := qwerty--imalison
 # default layout for this keyboard
 
 KEYBOARD_LAYOUTS := \
 	test \
 	arensito--ben \
 	qwerty--ben \
+	qwerty--imalison \
 	colemak--kinesis-mod \
 	dvorak--kinesis-mod \
 	qwerty--kinesis-mod
